a3b164f39c765ec69d677033dadb8a045528674b
Jhon Olarte <deibyolarte24@gmail.com>
2025-06-11 17:13:22 -0500

    Instancio las variables de la clase Cliente

--- source/uniandes/cupi2/videotienda/mundo/Cliente.java
+++ source/uniandes/cupi2/videotienda/mundo/Cliente.java
@@ -1,5 +1,163 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ * $Id: VideoTienda.java,v 1.1 2005/12/16 15:13:33 k-marcos Exp $
+ * Universidad de los Andes (Bogot� - Colombia)
+ * Departamento de Ingenier�a de Sistemas y Computaci�n 
+ * Licenciado bajo el esquema Academic Free License version 2.1 
+ *
+ * Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
+ * Ejercicio: n4_videotienda
+ * Autor: Katalina Marcos - Diciembre 2005
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+ */
+
 package uniandes.cupi2.videotienda.mundo;
 
+import java.util.ArrayList;
+
+/**
+ * clase representa un cliente de la videotienda.
+ */
 public class Cliente {
 
+	//-----------------------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------------------
+
+    /**
+     * Cédula del cliente
+     */
+    private String cedula;
+
+    /**
+     * Nombre del cliente
+     */
+    private String nombre;
+
+    /**
+     * Dirección del cliente
+     */
+    private String direccion;
+
+    /**
+     * Saldo disponible del cliente
+     */
+    private int saldo;
+
+    /**
+     * Copias alquiladas por el cliente
+     */
+    private ArrayList<Copia> darAlquiladas;
+    
+  //-----------------------------------------------------------------
+    // Constructor
+    //-----------------------------------------------------------------
+
+    /**
+     * Crea un nuevo cliente.
+     * @param cedula Cédula del cliente. cedula != null.
+     * @param nombre Nombre del cliente. nombre != null.
+     * @param direccion Dirección del cliente. direccion != null.
+     */
+    public Cliente(String cedula, String nombre, String direccion) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.saldo = 0;
+        this.darAlquiladas = new ArrayList<Copia>();
+    }
+    
+  //-----------------------------------------------------------------
+    // Métodos
+    //-----------------------------------------------------------------
+
+    /**
+     * Da la cédula del cliente.
+     * @return cédula del cliente.
+     */
+    public String darCedula() {
+        return cedula;
+    }
+    
+    /**
+     * Da el saldo disponible del cliente.
+     * @return saldo del cliente.
+     */
+    public int darSaldo() {
+    	return saldo;
+    }
+
+    /**
+     * Da el nombre del cliente.
+     * @return nombre del cliente.
+     */
+    public String darNombre() {
+        return nombre;
+    }
+
+    /**
+     * Da la dirección del cliente.
+     * @return dirección del cliente.
+     */
+    public String darDireccion() {
+        return direccion;
+    }
+
+    /**
+     * Alquila una copia para el cliente.
+     * @param copia Copia a alquilar. copia != null.
+     */
+    public void alquilarCopia(Copia copia) {
+    	
+    }
+    
+    /**
+     * Recarga el saldo del cliente.
+     * @param monto Monto que va a recargar. monto > 0.
+     */
+    public void cargarSaldo(int monto) {
+    	
+    }
+    
+    /**
+     * Descuenta el saldo del cliente.
+     * @param monto Monto que desconta. monto > 0.
+     */
+    public void descargarSaldo(int monto) {
+    	
+    }
+    
+
+    /**
+     * Retorna las copias alquiladas.
+     * @return las copias alquiladas.
+     */
+    public ArrayList<Copia> darAlquiladas() 
+    {
+        
+    }
+    
+    /**
+     * Devuelve una copia alquilada por el cliente.
+     * @param titulo Título de la película.
+     * @param numeroCopia Número de copia.
+     * @return devolver la copia.
+     * @throws Exception si el cliente no tiene la copia alquilada.
+     */
+    public Copia buscarPeliculaAlquilada(String pelicula, int codigo)
+    {
+    	
+    }
+    
+    /**
+     * devolver la copia que el cliente alquiló
+     * @param titulo Titulo de la pelicula
+     * @param numeroCopia Número de copia
+     * @throws Exception si el cliente no ha alquilado la copia
+     */
+    public void devolverCopia(String titulo, int numeroCopia) throws Exception 
+    {
+    	
+    }
+    
 }
